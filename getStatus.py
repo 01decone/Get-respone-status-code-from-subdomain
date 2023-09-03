c1720e8e29423b8f46e81018ed2b05682b93f213
Vivek Kumar Jaiswal <71717128+01decone@users.noreply.github.com>
2023-09-03 23:39:48 +0530

    This is not a final version yet

---
+++ getStatus.py
@@ -0,0 +1,28 @@
+import requests
+import threading
+import time
+
+
+print(
+'''
+=========================================================
+|                                                       |
+|                                                       |
+|         ██▓▒­░⡷⠂  GetStatus by Vivek ⠐⢾░▒▓██          |
+|                                                       |
+|                                                       |
+=========================================================
+'''
+    )
+file = open('domainlist.txt','r')
+def gstatus(x):
+  try:
+   s_code = requests.get('http://'+x)
+   print(x, s_code.status_code)
+  except:
+   print("Not resolved: " +x)
+   pass
+for x in file:
+  x = x.strip()
+  time.sleep(0.0)
+  threading.Thread(target=gstatus, args=(x,)).start()
